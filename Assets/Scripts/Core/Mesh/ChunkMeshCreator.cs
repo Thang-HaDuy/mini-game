2a5693067cb6098d96ebd67daa34d59af3718d38
haduythang <haduythang871@gmail.com>
2026-05-19 12:06:50 +0700

    clean

--- Assets/Scripts/Core/Mesh/ChunkMeshCreator.cs
+++ Assets/Scripts/Core/Mesh/ChunkMeshCreator.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using MiniGame.Core.Context;
 using MiniGame.Core.Interfaces;
 using UnityEngine;
 
@@ -10,161 +8,12 @@ namespace MiniGame.Core.Mesh
 {
     public class ChunkMeshCreator : IMeshBuilder
     {
-        public class CreateMesh
-        {
-            public int[,,] DataToDraw;
-            public Action<UnityEngine.Mesh> OnComplete;
-        }
-
-        #region FaceData
-
-        static readonly Vector3Int[] CheckDirections = new Vector3Int[]
-        {
-        Vector3Int.right,
-        Vector3Int.left,
-        Vector3Int.up,
-        Vector3Int.down,
-        Vector3Int.forward,
-        Vector3Int.back
-        };
-
-        static readonly Vector3[] RightFace = new Vector3[]
-        {
-        new Vector3(.5f, -.5f, -.5f),
-        new Vector3(.5f, -.5f, .5f),
-        new Vector3(.5f, .5f, .5f),
-        new Vector3(.5f, .5f, -.5f)
-        };
-
-        static readonly int[] RightTris = new int[]
-        {
-        0,2,1,0,3,2
-        };
-
-        static readonly Vector3[] LeftFace = new Vector3[]
-        {
-        new Vector3(-.5f, -.5f, -.5f),
-        new Vector3(-.5f, -.5f, .5f),
-        new Vector3(-.5f, .5f, .5f),
-        new Vector3(-.5f, .5f, -.5f)
-        };
-
-        static readonly int[] LeftTris = new int[]
-        {
-        0,1,2,0,2,3
-        };
-
-        static readonly Vector3[] UpFace = new Vector3[]
-        {
-        new Vector3(-.5f, .5f, -.5f),
-        new Vector3(-.5f, .5f, .5f),
-        new Vector3(.5f, .5f, .5f),
-        new Vector3(.5f, .5f, -.5f)
-        };
-
-        static readonly int[] UpTris = new int[]
-        {
-        0,1,2,0,2,3
-        };
-
-        static readonly Vector3[] DownFace = new Vector3[]
-        {
-        new Vector3(-.5f, -.5f, -.5f),
-        new Vector3(-.5f, -.5f, .5f),
-        new Vector3(.5f, -.5f, .5f),
-        new Vector3(.5f, -.5f, -.5f)
-        };
-
-        static readonly int[] DownTris = new int[]
-        {
-        0,2,1,0,3,2
-        };
-
-        static readonly Vector3[] ForwardFace = new Vector3[]
-        {
-        new Vector3(-.5f, -.5f, .5f),
-        new Vector3(-.5f, .5f, .5f),
-        new Vector3(.5f, .5f, .5f),
-        new Vector3(.5f, -.5f, .5f)
-        };
-
-        static readonly int[] ForwardTris = new int[]
-        {
-        0,2,1,0,3,2
-        };
-
-        static readonly Vector3[] BackFace = new Vector3[]
-        {
-        new Vector3(-.5f, -.5f, -.5f),
-        new Vector3(-.5f, .5f, -.5f),
-        new Vector3(.5f, .5f, -.5f),
-        new Vector3(.5f, -.5f, -.5f)
-        };
-
-        static readonly int[] BackTris = new int[]
-        {
-        0,1,2,0,2,3
-        };
-
-        #endregion
-
-        #region FaceUVData
-
-        static readonly int[] XUVOrder = new int[]
-        {
-        2, 3, 1, 0
-        };
-
-        static readonly int[] YUVOrder = new int[]
-        {
-        0, 1, 3, 2
-        };
-
-
-        static readonly int[] ZUVOrder = new int[]
-        {
-        3, 1, 0, 2
-        };
-
-        #endregion
-
-        private Dictionary<Vector3Int, FaceData> CubeFaces = new Dictionary<Vector3Int, FaceData>();
         private TextureLoader textureLoader;
         public bool Terminate;
 
         public ChunkMeshCreator(TextureLoader textureLoaderInstance)
         {
-            CubeFaces = new Dictionary<Vector3Int, FaceData>();
             textureLoader = textureLoaderInstance;
-
-            for (int i = 0; i < CheckDirections.Length; i++)
-            {
-                if (CheckDirections[i] == Vector3Int.up)
-                {
-                    CubeFaces.Add(CheckDirections[i], new FaceData(UpFace, UpTris, YUVOrder));
-                }
-                else if (CheckDirections[i] == Vector3Int.down)
-                {
-                    CubeFaces.Add(CheckDirections[i], new FaceData(DownFace, DownTris, YUVOrder));
-                }
-                else if (CheckDirections[i] == Vector3Int.forward)
-                {
-                    CubeFaces.Add(CheckDirections[i], new FaceData(ForwardFace, ForwardTris, ZUVOrder));
-                }
-                else if (CheckDirections[i] == Vector3Int.back)
-                {
-                    CubeFaces.Add(CheckDirections[i], new FaceData(BackFace, BackTris, ZUVOrder));
-                }
-                else if (CheckDirections[i] == Vector3Int.left)
-                {
-                    CubeFaces.Add(CheckDirections[i], new FaceData(LeftFace, LeftTris, XUVOrder));
-                }
-                else if (CheckDirections[i] == Vector3Int.right)
-                {
-                    CubeFaces.Add(CheckDirections[i], new FaceData(RightFace, RightTris, XUVOrder));
-                }
-            }
-
         }
 
         public IEnumerator CreateMeshFromData(
@@ -174,10 +23,7 @@ namespace MiniGame.Core.Mesh
         {
             MeshData meshData = new();
 
-            Task task = Task.Factory.StartNew(() =>
-            {
-                GenerateMesh(blocks, meshData);
-            });
+            Task task = Task.Run(() => GenerateMesh(blocks, meshData));
 
             yield return new WaitUntil(() => task.IsCompleted);
 
@@ -186,10 +32,7 @@ namespace MiniGame.Core.Mesh
             callback(mesh);
         }
 
-        private void GenerateMesh(
-            int[,,] blocks,
-            MeshData meshData
-        )
+        private void GenerateMesh(int[,,] blocks, MeshData meshData)
         {
             for (int x = 0; x < WorldGenerator.ChunkSize.x; x++)
             {
@@ -203,13 +46,7 @@ namespace MiniGame.Core.Mesh
             }
         }
 
-        private void TryBuildBlock(
-            int[,,] blocks,
-            MeshData meshData,
-            int x,
-            int y,
-            int z
-        )
+        private void TryBuildBlock(int[,,] blocks, MeshData meshData, int x, int y, int z)
         {
             if (blocks[x, y, z] == 0)
                 return;
@@ -220,45 +57,22 @@ namespace MiniGame.Core.Mesh
                 int ny = y + dir.y;
                 int nz = z + dir.z;
 
-                bool neighborSolid =
-                    ChunkMeshUtility.IsSolid(
-                        blocks,
-                        nx,
-                        ny,
-                        nz);
-
-                if (neighborSolid)
+                if (ChunkMeshUtility.IsSolid(blocks, nx, ny, nz))
                     continue;
 
                 BuildFace(blocks, meshData, x, y, z, dir);
             }
         }
 
-        private void BuildFace(
-            int[,,] blocks,
-            MeshData meshData,
-            int x,
-            int y,
-            int z,
-            Vector3Int direction
-        )
+        private void BuildFace(int[,,] blocks, MeshData meshData, int x, int y, int z, Vector3Int direction)
         {
             int blockId = blocks[x, y, z];
 
-            var texture =
-                textureLoader.Textures[blockId];
+            var texture = textureLoader.Textures[blockId];
+            var face = ChunkFaceLibrary.Faces[direction];
+            var uvs = texture.GetUVsAtDirectionT(direction);
 
-            var face =
-                ChunkFaceLibrary.Faces[direction];
-
-            Vector2[] uvs =
-                texture.GetUVsAtDirectionT(direction);
-
-            ChunkMeshBuilder.AddFace(
-                meshData,
-                face,
-                new Vector3(x, y, z),
-                uvs);
+            ChunkMeshBuilder.AddFace(meshData, face, new Vector3(x, y, z), uvs);
         }
 
         private UnityEngine.Mesh BuildUnityMesh(MeshData meshData)
@@ -266,11 +80,7 @@ namespace MiniGame.Core.Mesh
             UnityEngine.Mesh mesh = new();
 
             mesh.SetVertices(meshData.Vertices);
-            mesh.SetIndices(
-                meshData.Indices,
-                MeshTopology.Triangles,
-                0);
-
+            mesh.SetIndices(meshData.Indices, MeshTopology.Triangles, 0);
             mesh.SetUVs(0, meshData.UVs);
 
             mesh.RecalculateBounds();
@@ -279,7 +89,5 @@ namespace MiniGame.Core.Mesh
 
             return mesh;
         }
-
     }
-
 }
